1f0a0db12fda2979f37bdd3573c069e500a3be89
joaofrancisco212 <joaof211209@gmail.com>
2026-02-25 14:56:54 -0300

    25/02

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+document.write("<h1>Exercício 1 <h1/>")
+
+var numero = prompt("Digite um numero qualquer: ");
+
+var quadrado = Number(numero) * Number(numero);
+var cubo = Number(numero) * Number(numero) * Number(numero);
+
+document.write("<p>O numero digitado é " + numero+ "<p/>")
+document.write("<p>O quadrado do numero é " + quadrado+ "<p/>")
+document.write("<p>O cubo do numero é " + cubo+ "<p/>")
---
+++ script.js
@@ -0,0 +1,18 @@
+var nome = "Joao Francisco";
+
+var idade = 16;
+
+var altura = 1.87;
+
+console.log( nome );
+console.log( idade );
+console.log( altura );
+
+document.write("<h1>Olá, seja bem-vindo ao JavaScript</h1>")
+
+document.write( nome + "<br>");
+document.write( idade + "<br>");
+document.write( altura + "<br>" );
+
+var dias = idade * 365
+document.write("<p style= 'color : red' >Você já viveu " + dias + " dias<p/>")
